a79db91ab0c780cb9a83ec53f8918234da445ef4
Paul Smith <code@uvwxy.de>
2020-02-21 15:45:29 +0100

    initial commit

---
+++ src/mini-iot-client.cpp
@@ -0,0 +1,58 @@
+#include "mini-iot-client.h"
+
+MiniIotClient::MiniIotClient(const char* clientHostName, const char* serverHostName, const char* wifiSsid,
+                             const char* wifiPass) {
+  this->clientHostName = clientHostName;
+  this->serverHostName = serverHostName;
+  this->wifiSsid = wifiSsid;
+  this->wifiPass = wifiPass;
+  this->wifiWaitDelay = 4000;
+  this->debugStream = NULL;
+}
+
+void MiniIotClient::setWifiWaitDelay(uint16_t delay) { wifiWaitDelay = delay; }
+void MiniIotClient::setDebugStream(Stream* stream) { debugStream = stream; }
+
+void MiniIotClient::connectToWifi() {
+  WiFi.hostname(clientHostName);
+  WiFi.mode(WIFI_STA);
+  do {
+    if (debugStream != NULL) {
+      debugStream->print("Connecting to WiFi ");
+      debugStream->print(wifiSsid);
+      debugStream->println("...");
+    }
+    WiFi.begin(wifiSsid, wifiPass);
+    delay(wifiWaitDelay);
+  } while (WiFi.status() != WL_CONNECTED);
+}
+
+void MiniIotClient::checkWifi() {
+  if (debugStream != NULL) {
+    debugStream->println("Checking WiFi...");
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWifi();
+  }
+}
+
+int MiniIotClient::postData(String filename, String payload, boolean append, boolean tsprefix) {
+  HTTPClient http;
+  http.begin(String("http://") + serverHostName          //
+             + "/files/" + clientHostName                //
+             + "/" + filename                            //
+             + "?append=" + (append ? "true" : "false")  //
+             + "&tsprefix=" + (tsprefix ? "true" : "false"));
+  http.addHeader("Content-Type", "text/plain");
+  int code = http.POST(payload);
+  if (debugStream != NULL) {
+    http.writeToStream(debugStream);
+  }
+  http.end();
+  return code;
+}
+
+int MiniIotClient::save(String filename, String data) { return postData(filename, data, false, false); }
+int MiniIotClient::saveWithTimestamp(String filename, String data) { return postData(filename, data, false, true); }
+int MiniIotClient::append(String filename, String line) { return postData(filename, line, true, false); }
+int MiniIotClient::appendWithTimestamp(String filename, String line) { return postData(filename, line, true, true); }
---
+++ src/mini-iot-client.h
@@ -0,0 +1,34 @@
+#ifndef MINIIOTCLIENT_H
+#define MINIIOTCLIENT_H
+
+#include <Arduino.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+
+class MiniIotClient {
+ private:
+  const char* clientHostName;
+  const char* serverHostName;
+  const char* wifiSsid;
+  const char* wifiPass;
+  uint16_t wifiWaitDelay;
+  Stream* debugStream;
+
+  int postData(String filename, String payload, boolean append, boolean tsprefix);
+
+ public:
+  MiniIotClient(const char* clientHostName, const char* serverHostName, const char* wifiSsid, const char* wifiPass);
+  void setWifiWaitDelay(uint16_t delay);
+  void setDebugStream(Stream * stream);
+
+  void connectToWifi();
+  void checkWifi();
+
+  int save(String filename, String data);
+  int saveWithTimestamp(String filename, String data);
+
+  int append(String filename, String line);
+  int appendWithTimestamp(String filename, String line);
+};
+
+#endif
